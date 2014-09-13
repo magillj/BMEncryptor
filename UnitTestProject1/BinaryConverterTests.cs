090d66033620777736c174a69d4629b747c7b0cf
magillj <solidmagill@gmail.com>
2014-09-12 17:41:47 -0700

    Fixes to unit tests improving style and effectiveness

---
+++ UnitTestProject1/BinaryConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMEncryptor;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BinaryConverterTests
+    {
+        public readonly string SUPPORTED_CHARS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 .";
+
+        BinaryConverter converter = new BinaryConverter();
+        Random random = new Random();
+
+        [TestMethod]
+        public void convertToAndFromBinaryProperly()
+        {
+            string startText = "";
+
+            Assert.AreEqual(startText,
+                converter.convertBinaryToText(converter.convertTextToBinary(startText)));
+        }
+
+        [TestMethod]
+        public void handleUnsupportedChars()
+        {
+            string startText = "|~`!@#$%^&*()_-+=;:'";
+
+            Assert.AreEqual("                    ",
+                converter.convertBinaryToText(converter.convertTextToBinary(startText)));
+        }
+
+        [TestMethod]
+        public void convertsToBinaryFast()
+        {
+            string startText = "";
+            // Generate start text with a lot of random, acceptable chars
+            for (int i = 0; i < 10000; i++)
+            {
+                startText += SUPPORTED_CHARS[random.Next(0, SUPPORTED_CHARS.Length)];
+            }
+
+            DateTime before = DateTime.Now;
+            converter.convertBinaryToText(converter.convertTextToBinary(startText));
+            DateTime after = DateTime.Now;
+
+            bool testPassed = false;
+            if (before.Subtract(after) < TimeSpan.FromMilliseconds(5))
+                testPassed = true;
+            Assert.AreEqual(true, testPassed);
+        }
+    }
+}
---
+++ UnitTestProject1/EncryptionEngineTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMEncryptor;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class EncryptionEngineTests
+    {
+        EncryptionEngine en = new EncryptionEngine();
+
+        [TestMethod]
+        public void scrambleText()
+        {
+            bool passedTest = true;
+            string startText = "Hello. This is a test";
+            string endText = en.scrambleLetters(startText);
+
+            if (startText == endText)
+                passedTest = false;
+            for (int i = 0; i < startText.Length; i++)
+                if (!endText.Contains(startText.Substring(i, 1)))
+                    passedTest = false;
+
+            Assert.AreEqual(true, passedTest);
+        }
+
+        [TestMethod]
+        public void unScrambleText()
+        {
+            string text = "Hello this is a test";
+
+            string before = en.scrambleLetters(text);
+
+            string after = en.unScrambleLetters(before);
+
+            Assert.AreEqual(text, after);
+        }
+
+        [TestMethod]
+        public void randomizeAndDeRandomizeText()
+        {
+            bool[] key = {true,true,false,true,false,false};
+            string text = "Hello. This is a test";
+            string randomizedText = en.randomizeText(text, key);
+            string deRandomizedText = en.deRandomizeText(randomizedText, key);            
+
+            Assert.AreEqual(text, deRandomizedText);
+            
+        }
+    }
+}
